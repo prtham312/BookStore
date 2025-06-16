01d5e97075124212f1a5817c628ad32c0388a7b2
prtham312 <pb3488@srmist.edu.in>
2025-06-16 21:12:29 +0530

    [Pratham] : feat: implement profile dropdown and update appbar styling

--- src/app/components/appbar/appbar.component.ts
+++ src/app/components/appbar/appbar.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule } from '@angular/material/input';
-import { MatFormField } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-appbar',
-  imports: [MatToolbarModule , MatInputModule , MatIconModule],
+  imports: [MatToolbarModule , MatInputModule , MatIconModule , MatMenuModule , CommonModule],
   templateUrl: './appbar.component.html',
   styleUrl: './appbar.component.css'
 })
 export class AppbarComponent {
-
+  isProfileOpen = false;
+  toggleProfileDropdown() {
+  this.isProfileOpen = !this.isProfileOpen;
+}
 }
