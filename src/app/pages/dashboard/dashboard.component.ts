4d42e4fa11f654ac70443052ed324e09c6c216e7
prtham312 <pb3488@srmist.edu.in>
2025-06-16 15:07:30 +0530

    [Pratham] : feat: add dashboard component and appbar with styling
    - Implemented routing to redirect to the dashboard component.
    - Created a new appbar component with a custom toolbar design.
    - Added search functionality and user icons in the appbar.
    - Integrated Material Design components for the appbar.
    - Included Roboto font and Material Icons in the index.html.
    - Established basic structure for the dashboard component.
    - Added unit tests for the dashboard component.
    - Created an SVG logo for the application.

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AppbarComponent } from '../../components/appbar/appbar.component';
+
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [AppbarComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
